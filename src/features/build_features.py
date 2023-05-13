ae6eb9641dcb9e227dcfb5ecd02fa29fd1018d25
pranavsetpal <pranav.setpal@gmail.com>
2023-05-13 06:37:11 +0530

    initial commit

---
+++ src/features/build_features.py
@@ -0,0 +1,16 @@
+import h5py
+import torch
+
+def load_dataset(type):
+    dataset = h5py.File(f"./data/{type}.h5", 'r')
+
+    images = dataset['images'][:,:,:,[0,1,7,10]]
+    # Vegetation Index
+    images[:,:,:,0] = (dataset['images'][:,:,:,8] - dataset['images'][:,:,:,4]) /\
+                      (dataset['images'][:,:,:,8] + dataset['images'][:,:,:,4] + 1e-08)
+
+    images = torch.from_numpy(images / 10000)
+
+    agbd = torch.tensor(dataset['agbd'][:], dtype=torch.float64).unsqueeze(1)
+
+    return images,agbd
