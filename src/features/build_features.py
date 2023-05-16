9ab44361199badf5844c88139a53401283d83d7c
pranavsetpal <pranav.setpal@gmail.com>
2023-05-17 00:26:04 +0530

    augment data with rotations and flips

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -2,18 +2,41 @@ import h5py
 import numpy as np
 import torch
 
-def load_dataset(data):
-    dataset = h5py.File(f"./data/{data}.h5", 'r')
-
+def build_features(dataset):
     veg_index = (dataset['images'][:,:,:,[8]] - dataset['images'][:,:,:,[4]]) /\
                 (dataset['images'][:,:,:,[8]] + dataset['images'][:,:,:,[4]] + 1e-08)
 
-    features = np.concatenate([
+    features = torch.from_numpy(np.concatenate([
         dataset['images'][:,:,:,[1,7,10]] / 10000,
         veg_index
-    ], 3)
-    features = torch.from_numpy(features)
+    ], 3))
+
+    return features
+
+
+def augment_data(data):
+    return torch.cat([
+        data.rot90(0,[1,2]),            # Rotate 000, Flip Non
+        data.rot90(0,[1,2]).flip(1),    # Rotate 000, Flip Ver
+        data.rot90(0,[1,2]).flip(2),    # Rotate 000, Flip Hor
+        data.rot90(1,[1,2]),            # Rotate 090, Flip Non
+        data.rot90(1,[1,2]).flip(1),    # Rotate 090, Flip Ver
+        data.rot90(1,[1,2]).flip(2),    # Rotate 090, Flip Hor
+        data.rot90(2,[1,2]),            # Rotate 180, Flip Non
+        data.rot90(2,[1,2]).flip(1),    # Rotate 180, Flip Ver
+        data.rot90(2,[1,2]).flip(2),    # Rotate 180, Flip Hor
+        data.rot90(3,[1,2]),            # Rotate 270, Flip Non
+        data.rot90(3,[1,2]).flip(1),    # Rotate 270, Flip Ver
+        data.rot90(3,[1,2]).flip(2),    # Rotate 270, Flip Hor
+    ])
+
+
+def load_dataset(data_type):
+    dataset = h5py.File(f"./data/{data_type}.h5", 'r')
+
+    data = build_features(dataset)
+    data = augment_data(data)
 
     target = torch.tensor(dataset['agbd'][:], dtype=torch.float64).unsqueeze(1)
 
-    return features,target
+    return data,target
--- src/models/train_vit.py
+++ src/models/train_vit.py
@@ -4,15 +4,15 @@ from src.features.build_features import load_dataset
 
 torch.manual_seed(0)
 
-train_features, train_target = load_dataset('train')
-val_features, val_target = load_dataset('val')
+train_data, train_target = load_dataset('train')
+val_data, val_target = load_dataset('val')
 
-_, _, image_size, n_features = train_features.shape
+_, _, image_size, n_features = train_data.shape
 patch_size = 3
 
-model = ViT(image_size, patch_size, n_features, n_enc_blocks=4)
-model.fit(train_features, train_target,
-          val_features, val_target,
+model = ViT(image_size, patch_size, n_features, n_enc_blocks=2)
+model.fit(train_data, train_target,
+          val_data, val_target,
           epochs=30,
           batch_size=1138,
           lr=0.01
