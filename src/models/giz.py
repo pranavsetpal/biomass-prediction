e976c53616c5247870c8451c0be026040ec12192
pranavsetpal <pranav.setpal@gmail.com>
2023-05-21 19:27:44 +0530

    add giz starter notebook model in pytorch

---
+++ src/models/giz.py
@@ -0,0 +1,86 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+class Lasso(nn.Module):
+  def __init__(self, image_size, n_channels, alpha, dtype=torch.float64):
+    super().__init__()
+    n_features = image_size**2 * n_channels
+
+    self.alpha = alpha
+    self.weights = nn.Parameter(torch.randn(n_features, dtype=dtype))
+    self.biases = nn.Parameter(torch.zeros(1))
+
+    self.MSE = nn.MSELoss(reduction="sum")
+
+
+  def forward(self, inputs):
+    norm_inputs = (inputs-inputs.mean()) / inputs.std()
+    flattened_inputs = norm_inputs.reshape(len(norm_inputs), -1)
+
+    weights = self.weights.unsqueeze(0).repeat(len(inputs),1)
+    biases = self.biases.unsqueeze(0).repeat(len(inputs),1)
+    output = torch.bmm(flattened_inputs.unsqueeze(1), weights.unsqueeze(2)).squeeze(2) + biases
+
+    return output
+
+
+  def loss(self, inputs, targets):
+    preds = self(inputs)
+    return self.MSE(preds, targets) + self.alpha*(self.weights).abs().sum()
+
+  def fit(self, train_dataset, val_dataset, epochs, batch_size, lr=1e-2):
+    optimizer = torch.optim.Adam(self.parameters(), lr=lr)
+
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
+    val_dataloader = DataLoader(val_dataset, batch_size=batch_size)
+    for epoch in range(epochs):
+
+      train_loss = 0
+      for X,Y in train_dataloader:
+        optimizer.zero_grad()
+        loss = self.loss(X, Y)
+        train_loss += loss.item()
+
+        loss.backward()
+        optimizer.step()
+      train_loss /= len(train_dataset)
+
+      val_loss = 0
+      for X,Y in val_dataloader:
+        val_loss += self.loss(X, Y).item()
+      val_loss /= len(val_dataset)
+
+      if (val_loss < 20):
+        print("---------------------")
+        print("Num of epoches:", epoch)
+        print("\tMSE  = ", val_loss)
+        print("\tRMSE = ", val_loss**(1/2))
+        print("---------------------")
+        break
+
+      print(f"Epoch {epoch+1}/{epochs}:")
+      print(f"  Train: MSE={train_loss:.4f} RSME={train_loss**(1/2):.4f}")
+      print(f"  Val  : MSE={val_loss  :.4f} RSME={val_loss**(1/2)  :.4f}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,6 +1,7 @@
 import torch
-from src.models.model import ViT
 from src.data.make_dataset import ABCDataset
+# from src.models.model import DeiT
+from src.models.giz import Lasso
 
 torch.manual_seed(0)
 
@@ -12,10 +13,18 @@ val_dataset = ABCDataset("val", device)
 _, image_size, n_features = train_dataset[0][0].shape
 patch_size = 5
 
-model = ViT(image_size, patch_size, n_features, n_enc_blocks=2).to(device)
-model.fit(train_dataset,
+lasso = Lasso(image_size, n_features, 0.1).to(device)
+lasso.fit(train_dataset,
           val_dataset,
-          epochs=2,
-          batch_size=4552,
-          lr=0.001
+          epochs=30,
+          batch_size=25036,
+          lr=0.01
 )
+
+# model = DeiT(lasso, image_size, patch_size, n_features, n_enc_blocks=2).to(device)
+# model.fit(train_dataset,
+#           val_dataset,
+#           epochs=2,
+#           batch_size=4552,
+#           lr=0.001
+# )
